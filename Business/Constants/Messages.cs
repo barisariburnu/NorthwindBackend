0446b572dec9cbe7ee97f380e1976f513952bda8
BURSA\baris.ariburnu <barisariburnu@gmail.com>
2019-12-10 16:37:56 +0300

    First commit

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities.Concrete;
+using System.Runtime.Serialization;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün başarıyla eklendi";
+        public static string ProductDeleted = "Ürün başarıyla silindi";
+        public static string ProductUpdated = "Ürün başarıyla güncellendi";
+        public static string ProductNameAlreadyExists = "Ürün adı zaten sistemde kayıtlı";
+
+        public static string CategoryAdded = "Kategori başarıyla eklendi";
+        public static string CategoryDeleted = "Kategori başarıyla silindi";
+        public static string CategoryUpdated = "Kategori başarıyla güncellendi";
+
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Parola hatalı girildi";
+        public static string SuccessfulLogin = "Kullanıcı girişi başarılıyla yapıldı";
+        public static string UserAlreadyExists = "Kullanızı zaten kayıtlı";
+        public static string UserRegistered = "Kullanıcı başarıyla kayıt edildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+        public static string AuthorizationDenied = "Kullanıcı yetkisi yetersizdir";
+
+    }
+}
---
+++ Core/Utilities/Messages/AspectMessages.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Core.Utilities.Messages
+{
+    public class AspectMessages
+    {
+        public static string WrongValidationType = "Wrong Validation Type";
+        public static string WrongLoggerType = "Wrong Logger Type";
+        public static string InternalServerError = "Internal Server Error";
+    }
+}
